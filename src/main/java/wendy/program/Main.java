92a6d43bb354acf21c3a0ae7ef3a2137c12b8316
minji <mjkim.peach@gmail.com>
2024-05-17 17:00:57 +0900

    첫질문

--- src/main/java/wendy/program/Main.java
+++ src/main/java/wendy/program/Main.java
@@ -2,7 +2,7 @@ package wendy.program;
 import java.util.Scanner;
 
 public class Main {
-    public void main(String[] args){
+    public static void main(String[] args){
         System.out.println("안녕하세요. 방탈출 추천 프로그램입니다.");
         System.out.println("이름과 방탈출 경험 횟수를 입력해주세요");
         Scanner scan = new Scanner(System.in);

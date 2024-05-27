23ea13e8be45f915d5b8e62495fe98ab086c22e6
minji <mjkim.peach@gmail.com>
2024-05-27 22:18:08 +0900

    스레드 잔액 부족해도 계산하는 오류 수정

--- src/main/java/wendy/program/Bank.java
+++ src/main/java/wendy/program/Bank.java
@@ -7,14 +7,14 @@ public class Bank {
     }
 
     public synchronized void withdraw(int persons, int playPrice) {
-        if (money >= playPrice) {
+        if (money >= playPrice*persons) {
             for(int i=0; i<persons; i++) {
                 money -= playPrice;
                 System.out.println("1명 결제가 완료되었습니다. 잔액: " + money);
             }
             System.out.println("");
         } else {
-            System.out.println("잔액이 부족합니다. 결제를 진행할 수 없습니다.");
+            System.out.println(persons+"명의 금액을 결제할 잔액이 부족합니다. 결제를 진행할 수 없습니다.");
         }
     }
 }
